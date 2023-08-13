0945d3ac811cc967383af5c431ccca1253f20e6b
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:42:29 +0100

    [add] sortByDate func

---
+++ utils/sort.ts
@@ -0,0 +1,8 @@
+const sortByDates = (array: any[]): any => {
+    return array.sort((a, b) => {
+        const dateA: any = new Date(a.date)
+        const dateB: any = new Date(b.date)
+
+        return dateA - dateB;
+    })
+}
