835cc9052cbdf0b2832c8c2553c17fadded0c172
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:42:52 +0100

    ..

--- utils/RegEx.ts
+++
@@ -1,5 +0,0 @@
-const EMAIL_REGEX: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const USER_REGEX: RegExp = /^[a-zA-Z][a-zA-Z0-9-_]{2,23}$/
-const PSWD_REGEX: RegExp = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{7,89}$/
-
-export { EMAIL_REGEX, USER_REGEX, PSWD_REGEX }
