21d2423f0a80e40425efa0b92aa9ee2eee10ca0d
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:52:47 +0100

    [add] rate limiter

---
+++ middlewares/limiter.middleware.ts
@@ -0,0 +1,23 @@
+import { sendError } from '../utils/sendRes'
+import { Request, Response, NextFunction } from 'express'
+import rateLimit,
+{ RateLimitRequestHandler, Options } from 'express-rate-limit'
+
+export default function limit({
+    max,
+    timerArr,
+    msg = "Too many requests sent."
+}: ILimiter): RateLimitRequestHandler {
+    const limiter: RateLimitRequestHandler = rateLimit({
+        max, // max attempt
+        windowMs: timerArr[Math.floor(Math.random() * timerArr.length)] * 1000, // throttle
+        message: { msg },
+        handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
+            sendError(res, options.statusCode, options.message?.msg)
+        },
+        legacyHeaders: false,
+        standardHeaders: true,
+    })
+
+    return limiter
+}
