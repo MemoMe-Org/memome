d1d52cb32fde1d64b1d11eec97bd72048e0ec8ca
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:43:20 +0100

    [add] more env

--- env.d.ts
+++ env.d.ts
@@ -6,10 +6,13 @@ declare namespace NodeJS {
         DATABASE_URL: string
         BUCKET_REGION: string
         AWS_SECRET_ID: string
+        PLUNK_API_KEY: string
         SESSION_SECRET: string
         AWS_ACCESS_KEY: string
         GOOGLE_CLIENT_ID: string
+        GITHUB_CLIENT_ID: string
         GOOGLE_CLIENT_SECRET: string
+        GITHUB_CLIENT_SECRET: string
         NODE_ENV: 'production' | 'development'
     }
 }
