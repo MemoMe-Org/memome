f84eaaba9211176f1b2d534c88ff0d243f216b6b
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:43:48 +0100

    [add] auth route

---
+++ routes/auth.route.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express'
+import limit from '../middlewares/limiter.middleware'
+import { login } from '../controllers/login.controller'
+import { signup } from '../controllers/signup.controller'
+
+const router: Router = Router()
+
+router.post('/login', limit({
+    max: 5,
+    timerArr: [90, 60, 45, 75],
+    msg: 'Too many attempts. Try again later.'
+}), login)
+router.post('/signup', signup)
