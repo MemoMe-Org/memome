f5de0a0482025f2b4767a1ce6d5532240b2ed367
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 00:37:54 +0100

    [edit] email regex

---
+++ utils/RegExp.ts
@@ -0,0 +1,7 @@
+// const EMAIL_REGEX: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+const EMAIL_REGEX: RegExp = /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com|aol\.com|icloud\.com|hotmail\.com|mail\.com|live\.com|protonmail\.com|yandex\.com|zoho\.com|gmx\.com|fastmail\.com|inbox\.com|rocketmail\.com|rediffmail\.com|cox\.net|att\.net|comcast\.net)$/i;
+
+const USER_REGEX: RegExp = /^[a-zA-Z][a-zA-Z0-9-_]{2,23}$/
+const PSWD_REGEX: RegExp = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{7,89}$/
+
+export { EMAIL_REGEX, USER_REGEX, PSWD_REGEX }
