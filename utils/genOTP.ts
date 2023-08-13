1eb7aa4974aa7aec81577865db8925fe47160455
Raheem Kawojue <kawojue08@gmail.com>
2023-08-13 21:48:56 +0100

    [add] otp generator

---
+++ utils/genOTP.ts
@@ -0,0 +1,10 @@
+export default function generateOTP(): IGenOTP {
+    let totp: string = ''
+    const digits: string = '0918273645'
+    const length: number = parseInt('65'[Math.floor(Math.random() * 2)])
+    for (let i = 0; i < length; i++) {
+        totp += digits[Math.floor(Math.random() * length)]
+    }
+
+    return { totp, totp_expiry: Date.now() + 30 * 60 * 1000 }
+}
