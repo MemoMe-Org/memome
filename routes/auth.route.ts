1fe1506fd68443c96012d2dcc19971dac8199328
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 02:23:43 +0100

    upload middleware

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import upload from '../middlewares/upload.middleware'
 import limit from '../middlewares/limiter.middleware'
 import { login } from '../controllers/login.controller'
 import { signup } from '../controllers/signup.controller'
@@ -11,7 +12,7 @@ router.post('/login', limit({
     timerArr: [90, 60, 45, 75],
     msg: 'Too many attempts. Try again later.'
 }), login)
-router.post('/signup', signup)
+router.post('/signup', upload.single('avatar'), signup)
 router.get('/logout', logout)
 
 export default router
