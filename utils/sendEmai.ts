107157a05c4d67af6d90492d4641c508739b16c3
Raheem Kawojue <kawojue08@gmail.com>
2023-08-14 01:02:10 +0100

    [add] send email

---
+++ utils/sendEmai.ts
@@ -0,0 +1,11 @@
+import plunk from '../configs/plunk.config'
+
+const sendEmail = async ({ to, subject, body }: IMail) => {
+    try {
+        await plunk.emails.send({ to, subject, body })
+    } catch (err: unknown) {
+        console.log(err)
+    }
+}
+
+export default sendEmail
