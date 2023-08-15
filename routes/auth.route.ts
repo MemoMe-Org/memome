8176cecd0fa6c299e306a248d1b3608daafef38f
Raheem Kawojue <kawojue08@gmail.com>
2023-08-15 13:46:23 +0100

    [add] google login route

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -4,8 +4,11 @@ import limit from '../middlewares/limiter.middleware'
 import { login } from '../controllers/login.controller'
 import { signup } from '../controllers/signup.controller'
 import { logout } from '../controllers/logout.controller'
+import { passport } from '../controllers/google/callback.google'
 
 const router: Router = Router()
+
+const { CLIENT_URL } = process.env
 
 router.post('/login', limit({
     max: 5,
@@ -14,5 +17,20 @@ router.post('/login', limit({
 }), login)
 router.post('/signup', upload.single('avatar'), signup)
 router.get('/logout', logout)
+
+router.get(
+    '/google',
+    passport.authenticate('google', {
+        scope: ['email', 'profile']
+    })
+)
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {
+        session: false,
+        successRedirect: `${CLIENT_URL!}/profile`,
+        failureRedirect: `${CLIENT_URL!}/auth/login/failed`
+    })
+)
 
 export default router
