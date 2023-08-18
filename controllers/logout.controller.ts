11a9e9e072335e65f0607a4af5231a62ab32261b
Raheem Muyiwa <kawojue08@gmail.com>
2023-08-18 11:20:06 +0100

    Update logout.controller.ts

--- controllers/logout.controller.ts
+++ controllers/logout.controller.ts
@@ -2,7 +2,7 @@ import prisma from '../prisma'
 import getCookie from '../utils/getCookie'
 import { Request, Response } from 'express'
 import StatusCodes from '../utils/StatusCodes'
-const exoressAsyncHandler = require('express-async-handler')
+const expressAsyncHandler = require('express-async-handler')
 
 const clear = (req: Request, res: Response) => {
     const cookieNames = Object.keys(req.cookies)
@@ -13,7 +13,7 @@ const clear = (req: Request, res: Response) => {
     res.sendStatus(StatusCodes.NoContent)
 }
 
-const logout = exoressAsyncHandler(async (req: Request, res: Response) => {
+const logout = expressAsyncHandler(async (req: Request, res: Response) => {
     const authHeader = req.headers.cookie
     if (!authHeader) return clear(req, res)
 
