d29e7ae86abb7452eae771b19e048fece5f0b139
Raheem Kawojue <kawojue08@gmail.com>
2023-08-19 00:51:06 +0100

    [add] github sign in

--- routes/auth.route.ts
+++ routes/auth.route.ts
@@ -1,22 +1,29 @@
-import { Router } from 'express'
+import { Router, } from 'express'
+import { githubAuth } from '../controllers/github/'
 import limit from '../middlewares/limiter.middleware'
 import { login } from '../controllers/login.controller'
 import { signup } from '../controllers/signup.controller'
 import { logout } from '../controllers/logout.controller'
 import { passport } from '../controllers/google/callback.google'
+import { githubAuthCallback } from '../controllers/github/callback.github'
 
 const router: Router = Router()
 
 const { CLIENT_URL } = process.env
 
+
+// local sign in
 router.post('/login', limit({
     max: 5,
     timerArr: [90, 60, 45, 75],
     msg: 'Too many attempts. Try again later.'
 }), login)
 router.post('/signup', signup)
+
 router.get('/logout', logout)
 
+
+// Google sign in
 router.get(
     '/google',
     passport.authenticate('google', {
@@ -27,9 +34,14 @@ router.get(
     '/google/callback',
     passport.authenticate('google', {
         session: false,
+        failureRedirect: `${CLIENT_URL!}/login`,
         successRedirect: `${CLIENT_URL!}/profile`,
-        failureRedirect: `${CLIENT_URL!}/auth/login/failed`
     })
 )
+
+
+// Github sign in
+router.get('/github', githubAuth)
+router.get('/github/callback', githubAuthCallback)
 
 export default router
