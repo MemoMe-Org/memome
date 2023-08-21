4f5adbcb44c8a9aadc86cf0b3db019883d6d121a
Raheem Kawojue <kawojue08@gmail.com>
2023-08-21 17:06:22 +0100

    [add] otp request controller

---
+++ controllers/send-otp.controller.ts
@@ -0,0 +1,43 @@
+import prisma from '../prisma'
+import generateOTP from '../utils/genOTP'
+import { Request, Response } from 'express'
+import { EMAIL_REGEX } from '../utils/RegExp'
+import StatusCodes from '../utils/StatusCodes'
+import sendOTP from '../services/send-otp.mail'
+import { sendError, sendSuccess } from '../utils/sendRes'
+const expressAsyncHandler = require('express-async-handler')
+
+const sendOtp = expressAsyncHandler(async (req: Request, res: Response) => {
+    let { email } = req.body
+    email = email?.toLowerCase()?.trim()
+
+    if (!email || !EMAIL_REGEX.test(email)) {
+        sendError(res, StatusCodes.BadRequest, 'Invalid email.')
+        return
+    }
+
+    const user = await prisma.users.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'Account does not exist.')
+        return
+    }
+
+    const { totp, totp_expiry } = generateOTP()
+
+    await sendOTP(totp, email)
+
+    await prisma.users.update({
+        where: { email },
+        data: { totp, totp_expiry }
+    })
+
+    sendSuccess(res, StatusCodes.OK, {
+        success: true,
+        msg: 'An OTP code has been sent to your email.'
+    })
+})
+
+export { sendOtp }
