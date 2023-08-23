509fcef804e795cf436c704a9d0a3db78727fb7e
Raheem Kawojue <kawojue08@gmail.com>
2023-08-23 23:10:04 +0100

    [add] settings api

---
+++ controllers/api/settings.api.ts
@@ -0,0 +1,28 @@
+import prisma from '../../prisma'
+import { Request, Response } from 'express'
+import StatusCodes from '../../utils/StatusCodes'
+import { sendError, sendSuccess } from '../../utils/sendRes'
+const expressAsyncHandler = require('express-async-handler')
+
+const settings = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+
+    const user = await prisma.settings.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'Something went wrong.')
+        return
+    }
+
+    sendSuccess(res, StatusCodes.OK, {
+        user,
+        success: true,
+    })
+})
+
+export default settings
