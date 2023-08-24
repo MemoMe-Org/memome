c950095f7072f88b9b7f842ea3e6b7c70a8f78f9
Raheem Kawojue <kawojue08@gmail.com>
2023-08-24 14:31:02 +0100

    [edit]

--- utils/genOTP.ts
+++ utils/genOTP.ts
@@ -1,9 +1,8 @@
 import { IGenOTP } from '../interfaces'
 
-export default function generateOTP(): IGenOTP {
+export default function generateOTP(length: number = 5): IGenOTP {
     let totp: string = ''
     const digits: string = '0918273645'
-    const length: number = parseInt('65'[Math.floor(Math.random() * 2)])
     for (let i = 0; i < length; i++) {
         totp += digits[Math.floor(Math.random() * length)]
     }
