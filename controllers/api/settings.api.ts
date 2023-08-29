e0412438b5850f5805ee4b5b4b96c8be6eafd611
Raheem Kawojue <kawojue08@gmail.com>
2023-08-29 22:17:53 +0100

    [add] edit gen  msg type

--- controllers/api/settings.api.ts
+++ controllers/api/settings.api.ts
@@ -22,4 +22,88 @@ const settings = expressAsyncHandler(async (req: Request, res: Response) => {
     sendSuccess(res, StatusCodes.OK, { user })
 })
 
+
+const toggles = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+    const { type } = req.params
+    const { toggle } = req.body
+
+    const user = await prisma.users.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'Something went wrong.')
+        return
+    }
+
+    switch (type) {
+        case 'files':
+            await prisma.settings.update({
+                where: { userId },
+                data: {
+                    allow_files: toggle
+                }
+            })
+            break
+        case 'texts':
+            await prisma.settings.update({
+                where: { userId },
+                data: {
+                    allow_texts: toggle
+                }
+            })
+            break
+        case 'levels':
+            await prisma.settings.update({
+                where: { userId },
+                data: {
+                    show_levels: toggle
+                }
+            })
+            break
+    }
+
+    sendSuccess(res, StatusCodes.OK)
+})
+
+
+const messageType = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+    const { type } = req.query
+
+    const user = await prisma.users.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'Something is wrong.')
+        return
+    }
+
+    if (
+        type !== 'normal' && type !== 'all' &&
+        type !== 'nasty' && type !== 'relationship'
+    ) {
+        sendError(res, StatusCodes.BadRequest, 'Something is wrong.')
+        return
+    }
+
+    await prisma.settings.update({
+        where: { userId },
+        data: {
+            gen_msg_type: type
+        }
+    })
+
+    sendSuccess(res, StatusCodes.OK)
+})
+
 export default settings
+export { toggles, messageType }
