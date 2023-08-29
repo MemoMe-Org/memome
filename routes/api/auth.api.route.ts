a7ebc63337d1cc82bfedf975e207757394b56aad
Raheem Kawojue <kawojue08@gmail.com>
2023-08-29 22:41:30 +0100

    [add] settings route

--- routes/api/auth.api.route.ts
+++ routes/api/auth.api.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
+import settingsRoutes from './settings.api.route'
 import verifyUser from '../../middlewares/verifyUser'
 import profile from '../../controllers/api/profile.api'
 
 const router: Router = Router()
 
-router.get('/profile', verifyUser, profile)
+router.use(verifyUser)
+router.use('/settings', settingsRoutes)
+
+router.get('/profile', profile)
 
 export default router
