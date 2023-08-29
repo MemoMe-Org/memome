685028d0494229ad89cb4d334ff3cf8bc685b239
Raheem Kawojue <kawojue08@gmail.com>
2023-08-29 15:19:44 +0100

    [add] api msg routes

---
+++ routes/api/msg.api.route.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express'
+import verifyUser from '../../middlewares/verifyUser'
+import limit from '../../middlewares/limiter.middleware'
+import upload from '../../middlewares/upload.middleware'
+import sendMsg from '../../controllers/api/message.controller.api/send'
+import anonUser from '../../controllers/api/message.controller.api/check'
+import fetchMsg from '../../controllers/api/message.controller.api/fetch'
+import editMsgVisibility from '../../controllers/api/message.controller.api/edit'
+
+const router: Router = Router()
+
+router.route(
+    '/anon/:username',
+).get(
+    limit({
+        max: 1,
+        timerArr: [14, 9, 15],
+        msg: 'Denied by Cheat System.'
+    }),
+    anonUser
+).post(
+    upload.array('anon_files', 4),
+    sendMsg
+)
+
+router.get('/:userId', fetchMsg)
+router.get(
+    '/edit/:msgId',
+    verifyUser,
+    editMsgVisibility
+)
+
+export default router
