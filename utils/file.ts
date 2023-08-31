609b420320a744badfe121cddac597f5985c2ae7
Raheem Kawojue <kawojue08@gmail.com>
2023-08-31 10:06:47 +0100

    increase file mechanism

--- utils/file.ts
+++ utils/file.ts
@@ -8,19 +8,30 @@ mb - megabit * 1000
 MB - megabyte * 1024
 */
 
-const handleFile = (res: Response, file: any, maxSize: MaxSize): any => {
+const handleFile = (res: Response, file: any, maxSize: MaxSize, ...extensions: string[]): any => {
     const size: number = file.size
-    if (size < maxSize) {
-        const extension = file.originalname.split('.').pop()
-        return { ...file, extension }
-    } else {
+    const extension = file.originalname.split('.').pop()
+    const isAllowedExtension = extensions.includes(extension)
+
+    if (!isAllowedExtension) {
         sendError(
             res,
             StatusCodes.PayloadTooLarge,
-            `${file.originalname} is too large. > ${maxSize} bytes.`
+            `File extension is not allowed - ${file.originalname}`
         )
         return
     }
+
+    if (size > maxSize) {
+        sendError(
+            res,
+            StatusCodes.PayloadTooLarge,
+            `${file.originalname} is too large. > ${maxSize / (1024 * 1024)}MB.`
+        )
+        return
+    }
+
+    return { ...file, extension }
 }
 
 export default handleFile
