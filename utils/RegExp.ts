1b3b7befe379ae2dcf3bc0dacc8840234ce03aaf
Raheem Muyiwa <kawojue08@gmail.com>
2023-08-31 21:49:00 +0100

    Update RegExp.ts

--- utils/RegExp.ts
+++ utils/RegExp.ts
@@ -1,5 +1,5 @@
 // const EMAIL_REGEX: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-const EMAIL_REGEX: RegExp = /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com|icloud\.com|hotmail\.com)$/i;
+const EMAIL_REGEX: RegExp = /^[^\s@]+@(gmail\.com|yahoo\.com|outlook\.com|icloud\.com)$/i;
 
 const USER_REGEX: RegExp = /^[a-zA-Z][a-zA-Z0-9-_]{2,23}$/
 const PSWD_REGEX: RegExp = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{7,89}$/
