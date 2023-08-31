5ae817060cc67cc10a771a47749c7309928647b0
Raheem Kawojue <kawojue08@gmail.com>
2023-08-31 22:33:58 +0100

    ..

--- utils/getCookie.ts
+++
@@ -1,6 +0,0 @@
-export default function getCookie(authHeader: string, cookie: string) {
-    return authHeader
-        .split('; ')
-        .find((row: any) => row.startsWith(`${cookie}=`))
-        ?.split('=')[1]
-}
