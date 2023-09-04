c9c7566d3d24baa53ef08f5c1d244c0799de5485
Raheem Kawojue <kawojue08@gmail.com>
2023-09-04 16:35:01 +0100

    [add] edit avatar

--- controllers/api/profile.api.ts
+++ controllers/api/profile.api.ts
@@ -1,6 +1,11 @@
+import sharp from 'sharp'
 import prisma from '../../prisma'
+import handleFile from '../../utils/file'
 import { Request, Response } from 'express'
+import MaxSize from '../../enums/fileMaxSizes'
 import StatusCodes from '../../enums/StatusCodes'
+import genFileName from '../../utils/genFileName'
+import { deleteS3, getS3, uploadS3 } from '../../utils/s3'
 import { sendError, sendSuccess } from '../../utils/sendRes'
 const expressAsyncHandler = require('express-async-handler')
 
@@ -31,4 +36,117 @@ const profile = expressAsyncHandler(async (req: Request, res: Response) => {
     sendSuccess(res, StatusCodes.OK, { user })
 })
 
+const deleteAvatar = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+
+    const user = await prisma.users.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'User not found.')
+        return
+    }
+
+    const profile = await prisma.profiles.findUnique({
+        where: { userId }
+    })
+
+    if (!profile) {
+        sendError(res, StatusCodes.Conflict, 'Something went wrong.')
+        return
+    }
+
+    const path = profile.avatar?.path
+
+    if (path) {
+        try {
+            await deleteS3(path)
+        } catch {
+            sendError(res, StatusCodes.BadRequest, 'Something went wrong.')
+            return
+        }
+    }
+
+    await prisma.profiles.update({
+        where: { userId },
+        data: {
+            avatar: {
+                url: '',
+                path: '',
+                type: null,
+            }
+        }
+    })
+
+    sendSuccess(res, StatusCodes.OK, { msg: 'Done.' })
+})
+
+const changeAvatar = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+
+    const user = await prisma.users.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'User not found.')
+        return
+    }
+
+    const profile = await prisma.profiles.findUnique({
+        where: { userId }
+    })
+
+    if (!profile) {
+        sendError(res, StatusCodes.Conflict, 'Something went wrong.')
+        return
+    }
+
+    if (profile.avatar?.path) {
+        try {
+            await deleteS3(profile.avatar?.path)
+        } catch {
+            sendError(res, StatusCodes.BadRequest, 'Something went wrong.')
+        }
+    }
+
+    try {
+        const file = await handleFile(
+            res,
+            req.file,
+            MaxSize['5MB'],
+            'jpg', 'png',
+        )
+        const path = `Avatar/${user.id}/${genFileName()}.${file.extension}`
+        const url = await getS3(path)
+        const type = file.mimetype
+
+        const buffer = await sharp(file.buffer)
+            .resize({ fit: 'cover' })
+            .toBuffer()
+        await uploadS3(buffer, path, type)
+        await prisma.profiles.update({
+            where: { userId },
+            data: {
+                avatar: {
+                    url, path, type
+                }
+            }
+        })
+    } catch {
+        sendError(res, StatusCodes.BadRequest, 'Something went wrong.')
+        return
+    }
+
+    sendSuccess(res, StatusCodes.OK, { msg: 'Successfully changed.' })
+})
+
 export default profile
+export { deleteAvatar, changeAvatar }
