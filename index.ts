c991b54efdd27b2cbd846c8360d5f426e1c7ba54
Raheem Kawojue <kawojue08@gmail.com>
2023-09-05 11:11:15 +0100

    .

--- index.ts
+++ index.ts
@@ -63,8 +63,8 @@ app.use(passport.session())
 
 // intialize routes
 app.use('/auth', authRoute)
+app.use('/api/user/', userApi)
 app.use('/api/msg', msgApiRoute)
 app.use('/auth/api', authApiRoute)
-app.use('/api/user/', msgApiRoute)
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
