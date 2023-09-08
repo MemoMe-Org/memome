5b6f3cbe9e3ae81bb943e889c279f82876aa188f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 15:03:53 +0100

    [add] settings controller route

--- routes/api/account.api.route.ts
+++ routes/api/account.api.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import {
+import account, {
     editDisability, editUsername
 } from '../../controllers/api/account.api'
 
 const router: Router = Router()
 
+router.get('/', account)
 router.post('/username', editUsername)
 router.post('/disable', editDisability)
 
