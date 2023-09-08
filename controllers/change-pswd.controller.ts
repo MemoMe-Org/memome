f336907e62199e66fd6faa321bf0028ac6f92723
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 13:34:45 +0100

    [add] change pswd

---
+++ controllers/change-pswd.controller.ts
@@ -0,0 +1,56 @@
+import bcrypt from 'bcrypt'
+import prisma from '../prisma'
+import { Request, Response } from 'express'
+import { sendError } from '../utils/sendRes'
+import StatusCodes from '../enums/StatusCodes'
+import expressAsyncHandler from 'express-async-handler'
+
+const editPswd = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const username = req.username
+    const { oldPswd, pswd, pswd2 } = req.body
+
+    if (!oldPswd || !pswd || !pswd2) {
+        sendError(res, StatusCodes.BadRequest, 'All fields are required.')
+        return
+    }
+
+    if (pswd !== pswd2) {
+        sendError(res, StatusCodes.BadRequest, 'Passwords not match.')
+        return
+    }
+
+    const user = await prisma.users.findUnique({
+        where: { username }
+    })
+
+    if (!user) {
+        sendError(res, StatusCodes.NotFound, 'Account not found.')
+        return
+    }
+
+    const authMethod = user.auth_method
+    if (!user.password) {
+        sendError(
+            res,
+            StatusCodes.BadRequest,
+            `Password cannot be edited. You signed in with ${authMethod}.`
+        )
+        return
+    }
+
+    const isMatch: boolean = await bcrypt.compare(oldPswd, user.password)
+    if (!isMatch) {
+        sendError(res, StatusCodes.Unauthorized, 'Current password is incorrect.')
+        return
+    }
+
+    await prisma.users.update({
+        where: { username },
+        data: {
+            password: await bcrypt.hash(pswd, 10)
+        }
+    })
+})
+
+export { editPswd }
