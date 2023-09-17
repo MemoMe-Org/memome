8bf2bbc9063e4ff1f6916970fd3bc3ae3b2a3fd8
Raheem Kawojue <kawojue08@gmail.com>
2023-09-17 06:32:08 +0100

    [add] poll routes

--- routes/api/poll.api.route.ts
+++ routes/api/poll.api.route.ts
@@ -1,18 +1,25 @@
 import { Router } from 'express'
-import verifyUser from '../../middlewares/verifyUser.middleware'
-import { create } from '../../controllers/api/poll.controller.api/create'
 import upload from '../../middlewares/upload.middleware'
+import verifyUser from '../../middlewares/verifyUser.middleware'
+import { vote } from '../../controllers/api/poll.controller.api/vote'
+import { poll } from '../../controllers/api/poll.controller.api/poll'
+import { create } from '../../controllers/api/poll.controller.api/create'
+import fetchUserPolls from '../../controllers/api/poll.controller.api/fetch'
 
 const router: Router = Router()
 
+router.use(verifyUser)
 
 router.post(
     '/create',
-    [
-        verifyUser,
-        upload.array('poll_files', 4)
-    ],
+    upload.array('poll_files', 4),
     create
 )
+router.get('/get/:createdById/:pollId', poll)
+
+router.get('/fetch/:username', fetchUserPolls)
+
+router.post('/vote/:createdById/:pollId/:optionId', vote)
+
 
 export default router
