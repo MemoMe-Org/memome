2ad72845021868a7ddf3458f09f0f900a0c11b70
Raheem Kawojue <kawojue08@gmail.com>
2023-09-21 14:08:40 +0100

    ..

--- services/welcome.mail.ts
+++ services/welcome.mail.ts
@@ -1,10 +1,20 @@
+import path from 'path'
+import { readFileSync } from 'fs'
+import Handlebars from 'handlebars'
 import { sendNodeEmail } from '../helpers/sendEmail'
 
 const welcome = async (username: string, email: string) => {
+    const emailData: object = { username }
+    const templateSource = readFileSync(
+        path.join(__dirname, '../', './templates', '/welcome.hbs'), 'utf8'
+    )
+    const template = Handlebars.compile(templateSource)
+    const emailBody = template(emailData)
+
     await sendNodeEmail({
         to: email,
         subject: 'Welcome to MemoMe',
-        body: `.. coming back`
+        body: emailBody
     })
 }
 
