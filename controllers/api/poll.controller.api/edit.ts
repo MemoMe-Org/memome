857643c478b95161c526fa3402a1f9ba3c282812
Raheem Muyiwa <kawojue08@gmail.com>
2023-09-22 11:08:09 +0100

    [add] poll edit

---
+++ controllers/api/poll.controller.api/edit.ts
@@ -0,0 +1,44 @@
+import prisma from '../../../prisma'
+import { Request, Response } from 'express'
+import StatusCodes from '../../../enums/StatusCodes'
+import expressAsyncHandler from 'express-async-handler'
+import { sendError, sendSuccess } from '../../../helpers/sendRes'
+
+
+const edit = expressAsyncHandler(async (req: Request, res: Response) => {
+    // @ts-ignore
+    const userId = req.userId
+    const { pollId, type } = req.params
+
+    const poll = await prisma.poll.findUnique({
+        where: { userId, pollId },
+    })
+
+    if (!poll) {
+        sendError(res, StatusCodes.NotFound, 'Poll not found.')
+        return
+    }
+
+    let updatedPoll
+
+    switch (type) {
+        case 'active':
+            updatedPoll = await prisma.poll.update({
+                where: { userId, pollId },
+                data: {
+                    active: !poll.active
+                }
+            })
+            break
+        case 'visiblity':
+            updatedPoll = await prisma.poll.update({
+                where: { userId, pollId },
+                data: {
+                    private: !poll.private
+                }
+            })
+            break
+    }
+
+    sendSuccess(res, StatusCodes.OK)
+})
